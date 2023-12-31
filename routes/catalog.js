cd92a79bd73c7c41fb33772b4efaa70c9722070e
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-12-31 19:19:46 +0300

    add routes

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,2 +1,57 @@
 const express = require("express");
 const router = express.Router();
+
+const game_controller = require("../controllers/gameController");
+const developer_controller = require("../controllers/developerController");
+const publisher_controller = require("../controllers/publisherController");
+const genre_controller = require("../controllers/genreController");
+
+router.get("/", game_controller.index);
+
+router.get("/game/create", game_controller.game_create_get);
+router.post("/game/create", game_controller.game_create_post);
+
+router.get("/game/:id/delete", game_controller.game_delete_get);
+router.post("/game/:id/delete", game_controller.game_delete_post);
+
+router.get("/game/:id/update", game_controller.game_update_get);
+router.post("/game/:id/update", game_controller.game_update_post);
+
+router.get("/developer/create", developer_controller.developer_create_get);
+router.post("/developer/create", developer_controller.developer_create_post);
+
+router.get("/developer/:id/delete", developer_controller.developer_delete_get);
+router.post(
+  "/developer/:id/delete",
+  developer_controller.developer_delete_post
+);
+
+router.get("/developer/:id/update", developer_controller.developer_update_get);
+router.post(
+  "/developer/:id/update",
+  developer_controller.developer_update_post
+);
+
+router.get("/publisher/create", publisher_controller.publisher_create_get);
+router.post("/publisher/create", publisher_controller.publisher_create_post);
+
+router.get("/publisher/:id/delete", publisher_controller.publisher_delete_get);
+router.post(
+  "/publisher/:id/delete",
+  publisher_controller.publisher_delete_post
+);
+
+router.get("/publisher/:id/update", publisher_controller.publisher_update_get);
+router.post(
+  "/publisher/:id/update",
+  publisher_controller.publisher_update_post
+);
+
+router.get("/developer/create", genre_controller.genre_create_get);
+router.post("/genre/create", genre_controller.genre_create_post);
+
+router.get("/genre/:id/delete", genre_controller.genre_delete_get);
+router.post("/genre/:id/delete", genre_controller.genre_delete_post);
+
+router.get("/genre/:id/update", genre_controller.genre_update_get);
+router.post("/genre/:id/update", genre_controller.genre_update_post);
