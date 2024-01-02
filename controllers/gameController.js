e175fecbf60b4df5c2bd6b61699b954c3d376a29
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2024-01-02 16:37:05 +0300

    fix wrong naming of variable

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -41,11 +41,11 @@ exports.index = asyncHandler(async (req, res, next) => {
 });
 
 exports.game_list = asyncHandler(async (req, res, next) => {
-  const allGames = await Game.find().sort({ name: 1 }).exec();
+  const games = await Game.find().sort({ name: 1 }).exec();
 
   res.render("game_list", {
     title: "Games list",
-    games: allGames,
+    games: games,
   });
 });
 
